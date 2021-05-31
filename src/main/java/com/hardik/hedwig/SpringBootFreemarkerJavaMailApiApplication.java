3b65c19baf2f8fc6b882e6f3f80cd35988587e39
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-31 21:07:09 +0530

    added initial dummy project

---
+++ src/main/java/com/hardik/hedwig/SpringBootFreemarkerJavaMailApiApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.hedwig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFreemarkerJavaMailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFreemarkerJavaMailApiApplication.class, args);
+	}
+
+}
