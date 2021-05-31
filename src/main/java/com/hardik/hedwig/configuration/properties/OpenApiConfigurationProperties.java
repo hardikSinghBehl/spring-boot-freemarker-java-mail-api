21cbf9d2f1c7982f00fd430584981db4b14b0b55
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-31 21:31:15 +0530

    added open-API config classes

---
+++ src/main/java/com/hardik/hedwig/configuration/properties/OpenApiConfigurationProperties.java
@@ -0,0 +1,30 @@
+package com.hardik.hedwig.configuration.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties(prefix = "com.hardik.hedwig")
+public class OpenApiConfigurationProperties {
+
+	private Swagger swagger = new Swagger();
+
+	@Data
+	public class Swagger {
+		private String title;
+		private String description;
+		private String apiVersion;
+
+		private Contact contact = new Contact();
+
+		@Data
+		public class Contact {
+			private String email;
+			private String name;
+			private String url;
+		}
+
+	}
+
+}
