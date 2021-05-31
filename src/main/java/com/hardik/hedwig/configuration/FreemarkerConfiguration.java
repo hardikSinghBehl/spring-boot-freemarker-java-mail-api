45c4e714a93d60ccfd5d86e33840e9add3c96c5f
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-31 22:24:30 +0530

    added java-mail config classes

---
+++ src/main/java/com/hardik/hedwig/configuration/FreemarkerConfiguration.java
@@ -0,0 +1,19 @@
+package com.hardik.hedwig.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
+
+@Configuration
+public class FreemarkerConfiguration {
+
+	@Primary
+	@Bean
+	public FreeMarkerConfigurationFactoryBean bean() {
+		FreeMarkerConfigurationFactoryBean bean = new FreeMarkerConfigurationFactoryBean();
+		bean.setTemplateLoaderPath("classpath:/templates");
+		return bean;
+	}
+
+}
---
+++ src/main/java/com/hardik/hedwig/configuration/properties/EmailConfigurationProperties.java
@@ -0,0 +1,14 @@
+package com.hardik.hedwig.configuration.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties(prefix = "spring.mail")
+public class EmailConfigurationProperties {
+
+	private String username;
+	private String password;
+
+}
